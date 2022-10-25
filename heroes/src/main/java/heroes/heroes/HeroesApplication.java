8fd83c634701d15a03a96c80514b12b800643483
diealpa <73941471+diealpa@users.noreply.github.com>
2022-10-25 12:44:46 +0100

    Modifying files

--- heroes/src/main/java/heroes/heroes/HeroesApplication.java
+++ heroes/src/main/java/heroes/heroes/HeroesApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package heroes.heroes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class HeroesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HeroesApplication.class, args);
 	}
 
 }
--- heroes/src/test/java/heroes/heroes/HeroesApplicationTests.java
+++ heroes/src/test/java/heroes/heroes/HeroesApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package heroes.heroes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class HeroesApplicationTests {
 
 	@Test
 	void contextLoads() {
