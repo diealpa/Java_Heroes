3d2fa12511527774f7cc0ffd8361c0c9a0b4de3b
diealpa <73941471+diealpa@users.noreply.github.com>
2022-11-13 21:18:19 +0000

    Implementado filtro busqueda y limpieza ok

--- heroes/src/main/java/heroes/repository/HeroesRepository.java
+++ heroes/src/main/java/heroes/repository/HeroesRepository.java
@@ -9,7 +9,8 @@ import heroes.model.Heroe;
 
 public interface HeroesRepository extends JpaRepository<Heroe, Integer> {
 	
-	@Query("select h from Heroe h where h.nombre = ?1")
+
+	@Query("select h from Heroe h where h.nombre LIKE %?1% or h.id LIKE %?1% or h.superpoder LIKE %?1%")
 	public List<Heroe> buscarTodos(String busqueda);
 
 }
